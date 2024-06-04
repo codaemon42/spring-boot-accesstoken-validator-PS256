ff2ca8d5245571a28250f53ece2ff11088706e6f
naimk53 <naim@k53tech.com>
2024-06-04 16:40:13 +0600

    First Commit with spring boot 3 chassis

---
+++ src/main/java/com/codaemon/tokenvalidator/TokenvalidatorApplication.java
@@ -0,0 +1,13 @@
+package com.codaemon.tokenvalidator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokenvalidatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TokenvalidatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codaemon/tokenvalidator/TokenvalidatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codaemon.tokenvalidator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokenvalidatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
